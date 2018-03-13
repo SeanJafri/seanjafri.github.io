133d43cf2d631d42e1109c52f47dd766fdb3506c
SeanJafri <36804449+SeanJafri@users.noreply.github.com>
2018-03-13 18:59:00 -0500

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -70,59 +70,6 @@
     }).on("blur", ".floating-label-form-group", function() {
       $(this).removeClass("floating-label-form-group-with-focus");
     });
-  });
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-
-  
-   
-                var slideIndex = 1;
-                var z = document.getElementsByClassName("slideshow");
-                for (i = 0; i < z.length; i++) {
-                    //set custom data attribute to first current image index
-                    z[i].setAttribute("data-currentslide", 1);
-                    showDivs(z[i].getAttribute("data-currentslide"), i);
-                }
-                function plusDivs(n, j) {
-                    //get custom data attribute value of current image index to slideshow class index j
-                    slideIndex = parseInt(z[j].getAttribute("data-currentslide")[0]);
-                    showDivs(slideIndex += n, j);
-                }
-                function currentDiv(n, j) {
-                    showDivs(slideIndex = n, j); /* showDivs Not showSlides*/
-                }
-                function showDivs(n, j) {
-                    var i;
-                    var z = document.getElementsByClassName("slideshow")[j];
-                    var x = z.getElementsByClassName("mySlides");
-                    var dots = z.getElementsByClassName("dot");
-                    if (n > x.length) {
-                        slideIndex = 1
-                    }
-                    if (n < 1) {
-                        slideIndex = x.length;
-                    }
-                    //set custom data attribute to current image index
-                    z.setAttribute("data-currentslide", slideIndex);
-                    for (i = 0; i < x.length; i++) {
-                        x[i].style.display = "none";
-                    }
-                    for (i = 0; i < dots.length; i++) {
-                        dots[i].className = dots[i].className.replace(" active", "");
-                    }
-                    x[slideIndex - 1].style.display = "block";
-                    dots[slideIndex - 1].className += " active";
-                }
+  });       
           
 })(jQuery); // End of use strict
